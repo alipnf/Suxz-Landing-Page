2cb922f2f6152de8330e20e5cf0e8ffc78e34b17
alipnf <alifnur1224@gmail.com>
2023-07-10 23:13:25 +0700

    feat: Add event listeners to navbar links

--- src/js/script.js
+++ src/js/script.js
@@ -4,3 +4,11 @@ tham.addEventListener("click", () => {
   tham.classList.toggle("tham-active");
   navmenu.classList.toggle("hidden");
 });
+
+const navLinks = document.querySelectorAll("#navbar a");
+navLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    tham.classList.remove("tham-active");
+    navmenu.classList.add("hidden");
+  });
+});
